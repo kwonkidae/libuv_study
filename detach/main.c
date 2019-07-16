ba411ed09f5e0283a53bd453a1e9d3f5a1610b32
kwon <kkdosk@naver.com>
2019-07-16 11:16:15 +0900

    detach

---
+++ detach/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <uv.h>
+
+uv_loop_t *loop;
+uv_process_t child_req;
+uv_process_options_t options;
+
+int main() {
+  loop = uv_default_loop();
+
+  char* args[3] = {"sleep", "10", NULL};
+
+  options.exit_cb = NULL;
+  options.file ="sleep";
+  options.args = args;
+  options.flags = UV_PROCESS_DETACHED;
+
+  int r;
+  if ((r = uv_spawn(loop, &child_req, &options))) {
+    fprintf(stderr, "%s\n", uv_strerror(r));
+    return 1;
+  }
+
+  fprintf(stderr, "Launched sleep with PID %d\n", child_req.pid);
+  // uv_unref((uv_handle_t*)&child_req);
+
+  return uv_run(loop, UV_RUN_DEFAULT);
+}
