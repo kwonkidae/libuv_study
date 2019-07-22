34ab723d2592b510d664d43b41320cc8e4bec27a
kwon <kkdosk@naver.com>
2019-07-22 14:05:38 +0900

    named pipe

---
+++ pipe-echo-server/index.js
@@ -0,0 +1,12 @@
+var net = require('net');
+
+var stream = net.connect('/tmp/echo.sock');
+setTimeout(() => {
+  for(let i = 0; i < 1000; i++) {
+    stream.write(i + 'hello\n');
+  }
+}, 1000);
+
+stream.on('data', function(chunk){
+  console.log('recv:' + chunk);
+})
